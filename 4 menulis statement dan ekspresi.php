70aafe16202ef05d21c931a3de31322cfb1803e4
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-27 22:03:46 +0700

    membuat statement dan expresi

--- 4 menulis statement dan ekspresi.php
+++ 4 menulis statement dan ekspresi.php
@@ -1,18 +1,19 @@
 <?php
-echo "ini statement 1";
-echo "ini statement 2";
+echo "ini statement 1<p>";
+echo "ini statement 2<p>";
 //deklarasi sebuah variabel
-$b=15;
+$b=23;
 $c=7;
 //ekspresi
 $a1 =  $b * $c;
 $a2 =  $b + $c;
 // cetak ekspresi
-echo "<p> ini ekspresi ->  $a1 = $b * $c <p> ";
-echo "<p> ini ekpresi   -> $a1 = $b + $c <p> ";
-echo "<p> yaitu :" .$a1;
-echo "<p> yaitu :" .$a2;
+echo "<p>ini ekspresi -> $a1 = $b * $c <p> ";
+echo "<p>ini ekpresi  -> $a1 = $b + $c <p>";
+echo "<p> yaitu :". $a1;
+echo"<p> yaitu :". $a2;
 echo "<p> by syarifah nayla rizky </p>";
+
 echo "ini statement 1";
 echo "ini statement 2";
 
