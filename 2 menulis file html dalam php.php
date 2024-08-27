c1eb9f6f3b4c1a4eda1fb6f030a9ffc9cf8e6deb
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-27 13:55:58 +0700

    membuat html dalam php

---
+++ 2 menulis file html dalam php.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo "Belajar PHP" ?></title>
+</head>
+<body>
+<?php
+            echo "kita sedang belajar PHP<br>";
+            echo "<p>Belajar PHP hingga jadi master</p>";
+            echo "<p>by : Syarifah Nayla Rizky</p>";
+
+        ?>
+</body>
+</html>\
+
---
+++ 3 html dalam php.php
@@ -0,0 +1,11 @@
+<?php
+echo "<html>";
+echo "<head>";
+echo "<title>Judul Web</title>";
+echo "</head>";
+echo "<body>";
+echo "<p><h1>Selamat datang</h1><p>";
+echo "<p>ini adalah contoh file html didalam tag php</p>";
+echo "<p>by :syarifah nayla rizky</p>";
+echo "</body>";
+echo "</html>";
