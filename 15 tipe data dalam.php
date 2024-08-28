c56383acb5d0dc1f77d42751ce32d5e07c1d7a5b
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-28 11:44:41 +0700

    tipe data dalam php

---
+++ 15 tipe data dalam.php
@@ -0,0 +1,27 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin = 'perempuan';
+
+// tipe data string (teks)
+$nama_lengkap = "syarifah nayla rizky";
+
+// tipe data integer
+$umur = 16;
+
+// tipe data float
+$berat = 44;
+
+// tipe data float
+$tinggi = 157;
+
+// tipe data boolean
+$pelajar = false;
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "pelajar: $pelajar";
+?>
