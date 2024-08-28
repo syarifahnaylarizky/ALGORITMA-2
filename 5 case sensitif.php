1b89e005621cf6f275842d99b9f14301abe32264
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-28 09:06:23 +0700

    membuat case sensitif

--- 5 case sensitif.php
+++ 5 case sensitif.php
@@ -3,18 +3,18 @@
 $nama = "syarifah nayla rizky";
 $NAMAGURUSAYA = "BAPAK MAHDI";
 $umur = 16;
-const INI KONSTANTA = 123 ;
+const INI_KONSTANTA = 123 ;
 $ini_variabel = 23;
 $iniJugaVariabel = 49 ;
-$iniJugaVariabel ;
+$inijugavariabel ;
 $tahunlahir =08;
 
 echo "<p> nama saya adalah $nama";
 echo "<p> nama guru saya adalah $NAMAGURUSAYA";
 ECHO "<P> umur saya adalah $umur";
-echo "<P> saya lahir tahun $umur";
+echo "<p> saya lahir tahun $umur";
 
-echo
+echo 
 
 "<p> coba pakai blok program ",
 "<p> nama saya adalah $nama",
