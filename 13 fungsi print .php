6ce5e3fd65e6dcbc9b6f012db82fced51829d4a3
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-28 11:29:37 +0700

    fungsi print

---
+++ 13 fungsi print .php
@@ -0,0 +1,9 @@
+<?php
+// misalkan kita punya bilangan dengan notasi E seperti ini
+$harga = 100000;
+
+// jika kita cetak dengan echo:
+echo "Harganya adalah Rp $harga";
+
+// jika kita cetak dengan printf
+printf("Harganya adalah Rp %.2f", $harga);
