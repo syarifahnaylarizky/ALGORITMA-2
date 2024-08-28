0d86f5bd226ba52360abe0dcaff3792507d78c2e
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-28 11:03:11 +0700

    mencetak dengan echo php

---
+++ 09 mencetak dengan echo.php
@@ -0,0 +1,8 @@
+<?php
+
+// membuat variabel baru
+$nama_barang = "nasi goreng";
+$harga = 15000;
+$tempat = "warung makan"
+// menampilkan isi variabel
+echo "nayla membeli $nasi_goreng seharga Rp $harga di$tempat";
