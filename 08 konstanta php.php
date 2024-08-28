3fc0b1e53648ab2c9a145800111cab0167e54b85
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-28 10:48:53 +0700

    membuat konstanta

---
+++ 08 konstanta php.php
@@ -0,0 +1,11 @@
+<?php
+// membuat konstanta dengan fungsi define()
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'ahmadimuslim');
+define('DB_PASS', 'R4Hasia');
+define('DB_NAME', 'belajar');
+
+// membuat konstanta dengan kata kunci const
+const API_KEY = "1234";
+?>
+
