4d8dc19a27f9cfeccb9e6efc8cb7f345d59f41dd
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-28 11:23:17 +0700

    menggabungkan string atau teks

---
+++ 12 menggabungkan string atau teks.php
@@ -0,0 +1,10 @@
+<?php
+$txt1 = "Tutorial PHP";
+$txt2 = "syarifah nayla rizky";
+$x = 7;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Dibelajar pemrograman PHP di " . $txt2 . "<br>";
+echo $x + $y;
+?>
