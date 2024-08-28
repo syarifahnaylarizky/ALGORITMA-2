7588ecd27f98a2c0baafcc62dc08203e78d5f660
syarifahnaylarizky <raditsaid471@gmail.com>
2024-08-28 09:26:27 +0700

    penulisan komentar

---
+++ 6 penulisan komentar di php .php
@@ -0,0 +1,14 @@
+<?php
+
+// ini adalah komentar
+echo "Hello world";
+
+/*
+ini adalah komentar
+yang lebih dari satu
+baris
+*/
+echo "Kamu sudah dewasa";
+echo "<br> saya sudah makan asap";
+echo "<br> by : nayla";
+?>
